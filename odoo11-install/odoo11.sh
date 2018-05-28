c1a59e355ea2fca3053a3e9c119ffb8704e166af
odooerpdevelopers <odooerpdevelopers@gmail.com>
2018-05-28 03:11:43 +0200

    IMP script odoo 11 install, compatible ubuntu 18.04 now

--- odoo11-install/odoo11.sh
+++ odoo11-install/odoo11.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# Copyright 2018 odooerpcloud.com 
 
 ODOOVERSION=11.0
 DEPTH=10
 PATHBASE=~/Developments/odoo11
 PATHREPOS=~/Developments/odoo11/extra-addons
 
-wk32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-i386.deb"
-wk64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb"
+wk32="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-i386.tar.xz"
+wk64="https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz"
 
 # Update and install Postgresql
 sudo apt-get update
@@ -29,13 +30,16 @@ sudo apt-get install gcc python3-dev libxml2-dev libxslt1-dev \
 sudo apt-get -y install python3 python3-pip python-pip
 sudo pip3 install vobject qrcode num2words setuptools
 
+# FIX wkhtml* dependencie Ubuntu Server 18.04
+sudo apt-get install libxrender1
+
 # Install nodejs and less
-sudo apt-get install -y npm
+sudo apt-get install -y npm node-less
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g less
 
 # Download & install WKHTMLTOPDF
-rm wkhtml*
+rm $PATHBASE/wkhtml*
 if [ "`getconf LONG_BIT`" == "32" ];
 
 then
@@ -44,11 +48,13 @@ else
 	wget $wk64
 fi
 
-sudo dpkg --force-depends -i wkhtmltox*.deb
-sudo ln -s /usr/local/bin/wkhtml* /usr/bin
+tar xvf $PATHBASE/wkhtmltox*.tar.xz
+sudo cp -r $PATHBASE/wkhtmltox/bin/* /usr/local/bin/
+sudo cp -r $PATHBASE/wkhtmltox/bin/* /usr/bin/
+
 
 # install python requirements file (Odoo)
-# sed -i '/pyldap/d' $PATHBASE/odoo/requirements.txt
 sudo pip3 install -r $PATHBASE/odoo/requirements.txt
 
+echo "Odoo 11 Installation has finished!! ;) by odooerpcloud.com"
 
